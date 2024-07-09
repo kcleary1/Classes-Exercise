e4fa211820653e08ed97d1e2b8aa9ab7eec16cec
Kyle Cleary <kscleary11@gmail.com>
2024-07-09 10:40:45 -0600

    Exercise Complete

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car firstCar = new Car();
+            firstCar.Make = "Toyota";
+            firstCar.Model = "Corolla";
+            firstCar.Year = "1994";
+
+            Console.WriteLine($"My first car was a {firstCar.Year} {firstCar.Make} {firstCar.Model}.");
+            
+            
+            
         }
+
+       
     }
+
+
+
+
 }
